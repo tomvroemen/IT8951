02234be51da5d23b45dfe4e8fdd2526e61b4f620
Tom Vroemen <tomvroemen@gmail.com>
2019-07-15 14:55:41 +0200

    test

--- main.c
+++ main.c
@@ -31,8 +31,28 @@ int main (int argc, char *argv[])
 	sscanf(argv[2],"%d",&y);
 
 	IT8951_BMP_Example(x,y,argv[3]);
+
 	EPD_Text(0,  0, (uint8_t*)"hello world",0x00, 0xff);
+
+	IT8951WaitForDisplayReady();
 	
+	//Setting Load image information
+	stLdImgInfo.ulStartFBAddr    = (uint32_t)gpFrameBuf;
+	stLdImgInfo.usEndianType     = IT8951_LDIMG_L_ENDIAN;
+	stLdImgInfo.usPixelFormat    = IT8951_8BPP; 
+	stLdImgInfo.usRotate         = IT8951_ROTATE_0;
+	stLdImgInfo.ulImgBufBaseAddr = gulImgBufAddr;
+	//Set Load Area
+	stAreaImgInfo.usX      = 0;
+	stAreaImgInfo.usY      = 0;
+	stAreaImgInfo.usWidth  = 200;
+	stAreaImgInfo.usHeight = 200;
+	
+	//Load Image from Host to IT8951 Image Buffer
+	IT8951HostAreaPackedPixelWrite(&stLdImgInfo, &stAreaImgInfo);//Display function 2
+	
+	IT8951DisplayArea(0,0, gstI80DevInfo.usPanelW, gstI80DevInfo.usPanelH, 2);
+
 	IT8951_Cancel();
 
 	return 0;
