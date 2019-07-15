1d9c091723fa4afbf762f195a26de43ad52b3f49
Tom Vroemen <tomvroemen@gmail.com>
2019-07-15 20:56:59 +0200

    n

--- IT8951.c
+++ IT8951.c
@@ -808,13 +808,13 @@ void IT8951_BMP_Example(uint32_t x, uint32_t y,char *path)
 	stLdImgInfo.ulStartFBAddr    = (uint32_t)gpFrameBuf;
 	stLdImgInfo.usEndianType     = IT8951_LDIMG_L_ENDIAN;
 	stLdImgInfo.usPixelFormat    = IT8951_8BPP; 
-	stLdImgInfo.usRotate         = IT8951_ROTATE_90;
+	stLdImgInfo.usRotate         = IT8951_ROTATE_0;
 	stLdImgInfo.ulImgBufBaseAddr = gulImgBufAddr;
 	//Set Load Area
 	stAreaImgInfo.usX      = 0;
 	stAreaImgInfo.usY      = 0;
-	stAreaImgInfo.usWidth  = gstI80DevInfo.usPanelH;
-	stAreaImgInfo.usHeight = gstI80DevInfo.usPanelW;
+	stAreaImgInfo.usWidth  = gstI80DevInfo.usPanelW;
+	stAreaImgInfo.usHeight = gstI80DevInfo.usPanelH;
 
 	EPD_DrawLine(300, 200, 400, 316, 0x00);
 	EPD_DrawLine(325, 300, 400, 366, 0x00);
@@ -824,7 +824,7 @@ void IT8951_BMP_Example(uint32_t x, uint32_t y,char *path)
 	//Load Image from Host to IT8951 Image Buffer
 	IT8951HostAreaPackedPixelWrite(&stLdImgInfo, &stAreaImgInfo);//Display function 2
 	//Display Area ?V (x,y,w,h) with mode 2 for fast gray clear mode - depends on current waveform 
-	IT8951DisplayArea(0,0, gstI80DevInfo.usPanelH, gstI80DevInfo.usPanelW, 2);
+	IT8951DisplayArea(0,0, gstI80DevInfo.usPanelW, gstI80DevInfo.usPanelH, 2);
 }
 
 //-----------------------------------------------------------
