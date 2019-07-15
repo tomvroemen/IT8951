a6c1b791938b10be4103c88937d195191f5b102c
Tom Vroemen <tomvroemen@gmail.com>
2019-07-15 16:39:56 +0200

    lines

--- IT8951.c
+++ IT8951.c
@@ -808,6 +808,8 @@ void IT8951_BMP_Example(uint32_t x, uint32_t y,char *path)
 	stAreaImgInfo.usY      = 0;
 	stAreaImgInfo.usWidth  = gstI80DevInfo.usPanelW;
 	stAreaImgInfo.usHeight = gstI80DevInfo.usPanelH;
+
+	EPD_DrawLine(width/4, high/4, width/2, high/2, 0x40);
 	
 	//Load Image from Host to IT8951 Image Buffer
 	IT8951HostAreaPackedPixelWrite(&stLdImgInfo, &stAreaImgInfo);//Display function 2
